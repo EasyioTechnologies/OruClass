8422b2f4fa75a966e17294835a55a939f03a98ef
Burhanali2211 <easyio.tech@gmail.com>
2026-06-02 07:14:04 +0530

    fix: use IF NOT EXISTS for added columns in migration

--- apps/api/src/db/migrations/0001_freezing_tomas.sql
+++ apps/api/src/db/migrations/0001_freezing_tomas.sql
@@ -1,7 +1,7 @@
 
-ALTER TABLE "trainings" ADD COLUMN "labels" text[] DEFAULT '{}' NOT NULL;--> statement-breakpoint
-ALTER TABLE "trainings" ADD COLUMN "type" text DEFAULT 'in_person' NOT NULL;--> statement-breakpoint
-ALTER TABLE "trainings" ADD COLUMN "venue" text;--> statement-breakpoint
-ALTER TABLE "trainings" ADD COLUMN "meeting_link" text;--> statement-breakpoint
-ALTER TABLE "trainings" ADD COLUMN "start_date" timestamp;--> statement-breakpoint
-ALTER TABLE "trainings" ADD COLUMN "end_date" timestamp;
+ALTER TABLE "trainings" ADD COLUMN IF NOT EXISTS "labels" text[] DEFAULT '{}' NOT NULL;--> statement-breakpoint
+ALTER TABLE "trainings" ADD COLUMN IF NOT EXISTS "type" text DEFAULT 'in_person' NOT NULL;--> statement-breakpoint
+ALTER TABLE "trainings" ADD COLUMN IF NOT EXISTS "venue" text;--> statement-breakpoint
+ALTER TABLE "trainings" ADD COLUMN IF NOT EXISTS "meeting_link" text;--> statement-breakpoint
+ALTER TABLE "trainings" ADD COLUMN IF NOT EXISTS "start_date" timestamp;--> statement-breakpoint
+ALTER TABLE "trainings" ADD COLUMN IF NOT EXISTS "end_date" timestamp;
